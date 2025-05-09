8e64dd15e6468416d5209be4efd14233ea8f22dd
lilleyz7 <zxdxlx813@gmail.com>
2025-05-09 19:30:43 -0400

    Album and Band class

--- musicRanker/Entities/Album.cs
+++ musicRanker/Entities/Album.cs
@@ -2,5 +2,9 @@ namespace musicRanker.Entities;
 
 public class Album
 {
+    public required string AlbumName { get; set; }
+    public required int ReleaseYear { get; set; }
     
+    public required Band CreatedBy { get; set; }
+    public required int BandId { get; set; }
 }
--- musicRanker/Entities/Band.cs
+++ musicRanker/Entities/Band.cs
@@ -4,4 +4,6 @@ class Band
 {
     public required string Name { get; set; }
     public required int YearFormed { get; set; }
+
+    public required List<Album> Albums { get; set; } = new List<Album>();
 }
